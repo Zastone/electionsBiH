d51f5f86ace5fcc718d9a322dcf1096b0453fd16
Steve Nagoski <snagoski@techsoupglobal.org>
2014-08-20 13:30:39 +0200

    Initial import script and select statement scripts, table DDL

--- Database/create_ddl.sql
+++ Database/create_ddl.sql
@@ -1,1 +1,23 @@
 create database elections;
+
+use elections;
+
+create table municipalities (
+	municipality_id int,
+	municipality_name char(50),
+	kanton int,
+	parliament_bih int,
+	parliament_fbih int,
+	parliament_rs int,
+	president_bih int,
+	president_rs_election int,
+	PRIMARY KEY (municipality_id)
+	);
+
+create table muni (
+	municipality_id int,
+	municipality_name char(50),
+	PRIMARY KEY (municipality_id)
+	);
+
+
---
+++ Database/import_csv.sql
@@ -0,0 +1,12 @@
+use elections;
+
+-- quick test
+load data infile '/Users/stevenagoski/muni2.csv' 
+INTO muni 
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' 
+IGNORE 1 LINES;
+
+-- municipalities
+
+
+-- TBD
