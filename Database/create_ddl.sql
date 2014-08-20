92d11cf337593934cbc6cd54313d99fbe2a6bdd5
Steve Nagoski <snagoski@techsoupglobal.org>
2014-08-20 18:19:37 +0200

    loading municipalities & handling nulls complete

--- Database/create_ddl.sql
+++ Database/create_ddl.sql
@@ -1,7 +1,6 @@
 -- SQL Script that generates structure for Elections project
 
 -- drop database if exists `elections`;
-
 create database elections;
 
 use elections;
@@ -11,14 +10,29 @@ drop table if exists `municipalities`;
 create table municipalities (
 	municipality_id int,
 	municipality_name char(50),
-	kanton int,
-	parliament_bih int,
-	parliament_fbih int,
-	parliament_rs int,
-	president_bih int,
-	president_rs_election int,
+	kanton int NULL,
+	parliament_bih int NULL,
+	parliament_fbih int NULL,
+	parliament_rs int NULL,
+	president_bih int NULL,
+	president_rs_election int NULL,
 	PRIMARY KEY (municipality_id)
 	);
+
+drop table if exists `muni_temp`;
+
+create table muni_temp (
+	municipality_id int,
+	municipality_name char(50),
+	kanton char(4) NULL,
+	parliament_bih char(4) NULL,
+	parliament_fbih char(4) NULL,
+	parliament_rs char(4) NULL,
+	president_bih char(4) NULL,
+	president_rs_election char(4) NULL,
+	PRIMARY KEY (municipality_id)
+	);
+
 
 drop table if exists `muni`;
 
