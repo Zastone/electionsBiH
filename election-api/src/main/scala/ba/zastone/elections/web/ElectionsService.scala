00f3824f8cb8425980863802f1ea3756721caa36
Maciej Bilas <maciej.bilas@gmail.com>
2014-08-20 18:00:57 +0200

    Underscorize keys.

--- election-api/src/main/scala/ba/zastone/elections/web/ElectionsService.scala
+++ election-api/src/main/scala/ba/zastone/elections/web/ElectionsService.scala
@@ -3,8 +3,8 @@ package ba.zastone.elections.web
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-import ba.zastone.elections.model.Municipalities
-import org.json4s.DefaultFormats
+import ba.zastone.elections.model.{Municipality, Municipalities}
+import org.json4s.{FieldSerializer, DefaultFormats}
 import org.json4s.ext.JodaTimeSerializers
 import spray.httpx.Json4sJacksonSupport
 import spray.routing.HttpService
@@ -15,15 +15,31 @@ trait ElectionsService extends HttpService with Json4sJacksonSupport {
   implicit def json4sJacksonFormats = new DefaultFormats {
     override protected def dateFormatter =
       new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US)
-  } ++ JodaTimeSerializers.all
+  } ++ JodaTimeSerializers.all +
+  FieldSerializer[Municipality](serializer = {
+    case (name, v) => Some((Snakize.transform(name), v))
+  })
 
   protected def municipalitiesRoute = path("municipalities") {
     get {
-      complete{
+      complete {
         Municipalities.Example
       }
     }
   }
 
   protected def electionsRoute = municipalitiesRoute
+}
+
+private[web] object Snakize {
+  def transform(word: String): String = {
+    val spacesPattern = "[-\\s]".r
+    val firstPattern = "([A-Z]+)([A-Z][a-z])".r
+    val secondPattern = "([a-z\\d])([A-Z])".r
+    val replacementPattern = "$1_$2"
+    spacesPattern.replaceAllIn(
+      secondPattern.replaceAllIn(
+        firstPattern.replaceAllIn(
+          word, replacementPattern), replacementPattern), "_").toLowerCase
+  }
 }
