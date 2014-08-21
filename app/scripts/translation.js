dd04963a38c841a846f95b6c53cce1ebf4aae1d7
Michael Bauer <mihi@lo-res.org>
2014-08-21 13:16:58 +0200

    translation sort of works...

--- app/scripts/translation.js
+++ app/scripts/translation.js
@@ -1,14 +1,11 @@
-var translate;
-$(document).ready(
-    function() {
-        var lang=window.location.hash.replace("#","").split("/")[0] || "en";
+var translate= function() { var lang=window.location.hash.replace("#","").split("/")[0] || "en";
         var j = null;
-        $.ajax("data/translations/messages-"+lang+".json",{async: false}).done(function(d) {
-            var j=new Jed(d); });
-        translate= function(str) {
-            while (!j) {
+        return function(str) {
+            if (!j) {
+                $.ajax("data/translations/messages-"+lang+".json",{async: false}).done(function(d) {
+                        j=new Jed({locale_data: d}); });
                 //NOP
                 }
             return j.gettext(str);
                 }
-            });
+            }();
