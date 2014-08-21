1f93bef67b5189e56cb9b190247061c866d3bbde
Michael Bauer <mihi@lo-res.org>
2014-08-21 16:38:34 +0200

    created .po files

--- app/data/translations/createpo.py
+++ app/data/translations/createpo.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 import re
 import sys
+import json
 
 def findstrings(fn):
     with open(fn) as f:
@@ -8,9 +10,24 @@ def findstrings(fn):
 
 if __name__=="__main__":
     fn = sys.argv[1]
+    pd={}
+    if len(sys.argv)>2:
+        tf=sys.argv[2]
+        with open(tf) as f:
+            d=json.load(f)
+        pd=d["messages"]
+    
+    for k in pd.keys():
+        if k:
+            print(u"#: %s"%fn)
+            print(u'msgid "%s"'%k)
+            print(u'msgstr "%s"'%pd[k][1])
+            print(u'')
+
     for s in findstrings(fn):
-        print "#: %s"%fn
-        print 'msgid "%s"'%s
-        print 'msgstr ""'
-        print ""
+        if s not in k:
+            print("#: %s"%fn)
+            print('msgid "%s"'%s)
+            print('msgstr ""')
+            print("")
 
