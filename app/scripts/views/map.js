b8378ac1853719bcc743c84bcdf21b1c8419b695
Michael Bauer <mihi@lo-res.org>
2014-08-22 13:14:09 +0200

    get electoral units

--- app/scripts/views/map.js
+++ app/scripts/views/map.js
@@ -21,21 +21,12 @@ Electionsbih.Views = Electionsbih.Views || {};
         render: function () {
             //this.$el.html(this.template(this.model.toJSON()));
             console.log(this.collection)
-            console.log("Initialized the map view with this guy:",this.topojson)
-            // make electoral unit list
-            var electoral_units = _.unique(
-              _.flatten(
-                _.pluck(
-                  _.map(this.collection.models, function (x) {
-                      return x.get("electoral_units")
-                  }), "id")));
-            console.log(electoral_units)
-            var eu = {};
-            // find the elements (municipalities) of each electoral unit
-            // for each electoral unit id, construct a merged geojson by looping though the geojson features and merging those municipalities which match
-            _.each(electoral_units, function (x) {
 
-            });
+            var electoral_units = _.unique(_.flatten(
+                _.map(this.collection.models, function(d) {
+                    return _.pluck(d.get("electoral_units"),"id");
+                        })));
+            console.log(electoral_units);
         }
 
 
