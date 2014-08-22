033a67e2a71ea6308c82ea52162030784b38eb20
Drew Bollinger <drew@developmentseed.org>
2014-08-22 15:46:44 +0200

    adding map load

--- app/scripts/views/map.js
+++ app/scripts/views/map.js
@@ -10,14 +10,31 @@ Electionsbih.Views = Electionsbih.Views || {};
         initialize: function () {
             this.listenTo(this.collection, 'sync', this.render);
             this.layers = [];
+            var that = this;
+            $.getJSON("data/maps/bosnia.topojson",function(d) {
+              //that.geojson=topojson.feature(d,d.objects.bosnia);
+              that.topojson = d;
+            })
+
         },
 
         render: function () {
             //this.$el.html(this.template(this.model.toJSON()));
             console.log(this.collection)
+            console.log("Initialized the map view with this guy:",this.topojson)
+            // make electoral unit list
+            var electoral_units = _.unique(
+              _.flatten(
+                _.pluck(
+                  _.map(this.collection.models, function (x) {
+                      return x.get("electoral_units")
+                  }), "id")));
+            console.log(electoral_units)
+            var eu = {};
+            // find the elements (municipalities) of each electoral unit
+            // for each electoral unit id, construct a merged geojson by looping though the geojson features and merging those municipalities which match
+            _.each(electoral_units, function (x) {
 
-            _.each(this.collection.models, function (x) {
-            //  make a circle
             });
         }
 
