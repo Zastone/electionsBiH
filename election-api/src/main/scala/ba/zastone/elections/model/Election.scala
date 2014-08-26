48c76b90f269acbd904baf25d25f2df96dcb6158
Maciej Bilas <maciej.bilas@gmail.com>
2014-08-27 00:00:39 +0200

    Election and ElectionTypes classes are now in one file.

--- election-api/src/main/scala/ba/zastone/elections/model/Election.scala
+++ election-api/src/main/scala/ba/zastone/elections/model/Election.scala
@@ -4,3 +4,15 @@ import ba.zastone.elections.model.ElectionTypes.ElectionType
 
 
 case class Election(electionType: ElectionType, year: Int)
+
+object ElectionTypes extends Enumeration {
+  type ElectionType = Value
+
+  val Kanton = Value("kanton")
+  val ParlimentBih = Value("parliament_bih")
+  val parliamentFbih = Value("parliament_fbih")
+  val parliamentRs = Value("parliament_rs")
+  val presidentBis = Value("president_bih")
+  val presidentRs = Value("president_rs")
+
+}
