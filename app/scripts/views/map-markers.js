9c7730890288db9ca82f2faacaa84231d055dc97
Drew Bollinger <drew@developmentseed.org>
2014-09-19 15:07:04 -0400

    hover info works now

--- app/scripts/views/map-markers.js
+++ app/scripts/views/map-markers.js
@@ -19,25 +19,26 @@ Electionsbih.Views = Electionsbih.Views || {};
             this.listenTo(this.collection.mandates, 'sync', this.load);
 
             // add info div for hover info
-            var info = L.control();
+            this.info = L.control();
 
-            info.onAdd = function (map) {
+            this.info.onAdd = function (map) {
                 this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
                 this.update();
                 return this._div;
             };
 
-            info.update = function (props) {
-                this._div.innerHTML = (props ? '<h4>Municipality: </h4>' + props.muni + '<br><h5>Party: </h5>' + props.party + ', Votes: ' + props.votes :
-                'hover for more info');
+            this.info.update = function (props) {
+                this._div.innerHTML = (props ? 'Municipality: ' + props.municipality + '<br>Party: ' + props.party + ', Votes: ' + props.votes + ' (' + props.per + '%)' :
+                'Hover over markers for more info');
             };
 
-            info.addTo(Electionsbih.map);
+            this.info.addTo(Electionsbih.map);
         },
 
         render: function (ps) {
-
-            var partyStatus = ps || 0;
+            var that = this,
+            info = this.info,
+            partyStatus = ps || 0;
 
             var activeParties = _.pluck(_.filter(partyStatus, function(x) {
               return x['status'] === 'active';
@@ -51,7 +52,7 @@ Electionsbih.Views = Electionsbih.Views || {};
             var zp = function(n,c) { var s = String(n); if (s.length< c) { return zp("0" + n,c) } else { return s } }
 
             var partySeats = Electionsbih.router.partyCalc('country',this.collection.mandates,this.collection.results);
-            console.log(partySeats)
+
             var muni = this.centroids;
 
             var circles = [],
@@ -64,7 +65,7 @@ Electionsbih.Views = Electionsbih.Views || {};
               var resultsSort = _.sortBy(results, function(y) {
                 return -y['votes']
               })
-
+              var muniVotes = _.reduce(_.pluck(resultsSort,'votes'), function(a, b){ return a + b; }, 0)
               var topThree = resultsSort.slice(0,3)
 
               // let's find the municipality centroid as a latLng
@@ -79,7 +80,7 @@ Electionsbih.Views = Electionsbih.Views || {};
                 var jiggle = (Electionsbih.map.getBounds()._northEast.lat - Electionsbih.map.getBounds()._southWest.lat)/200
 
                 _.each(topThree, function(y,i){
-
+                   var per = (y['votes']/muniVotes * 100).toFixed(2);
                    if ((partyStatus === 0 || _.contains(activeParties,y['abbreviation'])) && _.contains(_.pluck(partySeats,'abbreviation'),y['abbreviation'])) {
                      circles.push(L.circleMarker([coord[1]+jiggle*Math.sin((i*2*Math.PI)/3),coord[0]+jiggle*Math.cos((i*2*Math.PI)/3)], {
                             radius: Math.sqrt(y['votes']) / 10,
@@ -88,7 +89,16 @@ Electionsbih.Views = Electionsbih.Views || {};
                             opacity: 1,
                             fillColor: color(y['abbreviation']),
                             fillOpacity: 1,
-                     }));
+                            values: {party: y['abbreviation'], votes: y['votes'], municipality: x.get('name'), per: per}
+                     })
+                     .on('mouseover', function (e) {
+                       var layer = e.target;
+                       info.update(layer.options.values);
+                     })
+                     .on('mouseout', function (e) {
+                       info.update();
+                     })
+                     );
                    }
                 })
 
@@ -97,33 +107,13 @@ Electionsbih.Views = Electionsbih.Views || {};
 
             // add circle layer
 
-            circleLayer = new L.LayerGroup(circles, {onEachFeature: this.onEachFeature}).addTo(Electionsbih.map);
-            console.log(circleLayer)
+            circleLayer = new L.LayerGroup(circles).addTo(Electionsbih.map);
             this.layers.push(circleLayer);
 
         },
 
         load: function () {
           this.loaded === 0 ? this.loaded += 1 : this.render();
-        },
-
-        onEachFeature: function (feature, layer) {
-          console.log(this)
-          layer.on({
-            mouseover: this.markerInfo,
-            mouseout: this.resetInfo
-          });
-        },
-
-        markerInfo: function (e) {
-          console.log("I'm hovering!")
-          var layer = e.target;
-          info.update(layer.feature.properties);
-        },
-
-        resetInfo: function (e) {
-          circleLayer.resetStyle(e.target);
-          info.update();
         }
 
     });
