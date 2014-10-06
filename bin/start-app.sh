be5390331a2ba562601e721a9b573a5aca535f9d
Maciej Bilas <maciej.bilas@gmail.com>
2014-10-06 21:00:49 +0200

    Logging.
    Fixes #45.

--- bin/start-app.sh
+++ bin/start-app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 nohup java -Dconfig.file=prod.conf \
+ -Dlogback.configurationFile=logback.prod.xml \
  -cp election-api/target/scala-2.11/elections-api-assembly-1.0.jar \
  ba.zastone.elections.web.ElectionsWeb >/dev/null 2>&1 & echo $! > pids/election-api.pid
--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :linked_files, [fetch(:_production_conf), 'db-pass']
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, ['pids']
+set :linked_dirs, ['pids', 'logs']
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
