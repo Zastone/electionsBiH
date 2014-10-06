1b771eb0a3904d5dc98f0edd146d0cf60d974869
Maciej Bilas <maciej.bilas@gmail.com>
2014-10-06 20:22:12 +0200

    deploy.rb: fix deploy:restart.

--- config/deploy.rb
+++ config/deploy.rb
@@ -85,17 +85,6 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :publishing, :restart
-
-  before :publishing, 'sbt:assembly'
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
       within release_path do
         execute 'bin/stop-app.sh'
         sleep 2
@@ -103,5 +92,8 @@ namespace :deploy do
       end
     end
   end
+
+  after :publishing, :restart
+  before :publishing, 'sbt:assembly'
 
 end
