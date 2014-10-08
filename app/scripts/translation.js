2222c6c99054e41d2ba55db91f284811f7dc5f29
Maciej Bilas <maciej.bilas@gmail.com>
2014-10-09 01:46:22 +0200

    [minor] translation.js formatting.

--- app/scripts/translation.js
+++ app/scripts/translation.js
@@ -1,14 +1,15 @@
-function createTranslation(lang) { 
-        var j = null;
-        return function(str) {
-            if (!j) {
-                $.ajax("data/translations/messages-"+lang+".json",{async: false}).done(function(d) {
-                        j=new Jed({locale_data: {messages: d}}); });
-                //NOP
-                }
-            return j.gettext(str);
-                }
-            }
+function createTranslation(lang) {
+    var j = null;
+    return function (str) {
+        if (!j) {
+            $.ajax("data/translations/messages-" + lang + ".json", {async: false}).done(function (d) {
+                j = new Jed({locale_data: {messages: d}});
+            });
+            //NOP
+        }
+        return j.gettext(str);
+    }
+}
 
-var translate= createTranslation( 
-    window.location.hash.replace("#","").split("/")[0] || "en");
+var translate = createTranslation(
+        window.location.hash.replace("#", "").split("/")[0] || "en");
